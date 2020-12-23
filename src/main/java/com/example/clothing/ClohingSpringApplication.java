eceae36fcd5f0d60575d6545018a1805a4b8148c
Alan Wang <alanwang53@gmail.com>
2020-12-22 16:41:56 -0800

    hello world

--- src/main/java/com/example/clothing/ClohingSpringApplication.java
+++ src/main/java/com/example/clothing/ClohingSpringApplication.java
@@ -8,6 +8,8 @@ public class ClohingSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClohingSpringApplication.class, args);		
+
+		System.out.println("Hello World");
 	}
 
 }
