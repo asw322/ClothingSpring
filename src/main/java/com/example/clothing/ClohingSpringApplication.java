cacac07810906413af59a19fa3aec084d4a149b1
Alan Wang <alanwang53@gmail.com>
2020-12-22 16:38:40 -0800

    initial push

---
+++ src/main/java/com/example/clothing/ClohingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClohingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClohingSpringApplication.class, args);		
+	}
+
+}
