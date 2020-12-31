2da4a0bcea401646ed044a3df4f9918c8591b879
Alan Wang <alanwang53@gmail.com>
2020-12-30 23:33:53 -0800

    Added ProcessingInterface

---
+++ src/main/java/com/example/clothing/ProcessingInterface.java
@@ -0,0 +1,18 @@
+/**
+ * Purpose of ProcessingInterface: 
+ * Process the data that flows between the ClientInterface and the DAO classes. 
+ * This class is also a part of the logical layer of the program. 
+ * 
+ * High level use cases: 
+ * 1. Takes returned objects from DAO, cleans it (processing), and returns to ClientInterface
+ * 2. Builds out the client request from ClientInterface
+ */
+
+package com.example.clothing;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessingInterface {
+    
+}
