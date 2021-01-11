515ae38bc884422bd5e19d383719532ee6a03812
Alan Wang <alanwang53@gmail.com>
2021-01-10 22:23:20 -0800

    tested product insertion API

--- src/test/java/com/example/clothing/ClohingSpringApplicationTests.java
+++ src/test/java/com/example/clothing/ClohingSpringApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.clothing;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClohingSpringApplicationTests {
 
+	@Autowired
+	private ProcessingInterface PROCESSING_INTERFACE;
+
 	@Test
 	void contextLoads() {
+		testInsertProductTokens();
+	}
+
+	void testInsertProductTokens() {
+		ArrayList<ProductToken> product_arr = new ArrayList<>();
+
+		product_arr.add(
+			new ProductToken(			// Sample product
+				"P2",
+				"test2",
+				"test2",
+				"test2",
+				"test2",
+				11.00,
+				"test2",
+				"test2",
+				"test2",
+				"test2",
+				"test2",
+				"test2",
+				null
+			)
+		);
+
+		System.out.println("Adding product");
+		int totalAdded = PROCESSING_INTERFACE.insertProductToken(product_arr);
+		System.out.println("Finished adding " + totalAdded + " product");
 	}
 
 }
